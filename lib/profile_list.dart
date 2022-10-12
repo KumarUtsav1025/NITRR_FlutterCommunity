5edd5df90f25e5706b338a2772e770a9b1d55bd6
Chandrakant0110 <chandrakant.21.07.2003@gmail.com>
2022-10-12 17:50:41 +0530

    added chandrakant

--- lib/profile_list.dart
+++ lib/profile_list.dart
@@ -4,6 +4,7 @@ import 'modals/profile.dart';
 class ProfileList {
   List<Profile> profileList = [
     Profile(name: 'Kumar Utsav', branch: Branch[5], picNum: 8),
+    Profile(name: 'Chandrakant Sahu', branch: Branch[8], picNum: 7),
     // Add your Name, branch & picNumber here
   ];
 }
