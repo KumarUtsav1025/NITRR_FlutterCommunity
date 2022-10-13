a3be53efd9588a6c1fa9642b6bea5307d80c70dc
Leonardo Maier <leonarmaier@gmail.com>
2022-10-13 10:14:00 -0300

    adds name to profile list

--- lib/profile_list.dart
+++ lib/profile_list.dart
@@ -5,6 +5,7 @@ class ProfileList {
   List<Profile> profileList = [
     Profile(name: 'Kumar Utsav', branch: Branch[5], picNum: 8),
     Profile(name: 'Chandrakant Sahu', branch: Branch[8], picNum: 7),
+    Profile(name: 'Leonardo Maier', branch: Branch[10], picNum: 10),
     // Add your Name, branch & picNumber here
   ];
 }
